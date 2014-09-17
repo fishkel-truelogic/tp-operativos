6ebdc7ef3855583a84f240d358d25b10ac83ed02
cesarcappetto <utnso@utnso.(none)>
2014-09-16 22:40:36 -0300

    #23 : Se finalizan las librerias para uso de sockets y se modifica archivo fereTypes

--- fereCommons/src/fereTypes.h
+++ fereCommons/src/fereTypes.h
@@ -24,8 +24,10 @@ static unsigned char const TRUE = 1;
 //CADENAS VARIABLES
 typedef char* String;
 
+
 //LOGICO
 typedef unsigned char Boolean;
+
 
 //VALOR 8-BITS NO SIGNADO
 typedef unsigned char Byte;
@@ -45,11 +47,24 @@ typedef unsigned short Int16U;
 //VALOR 16-BITS SIGNADO
 typedef signed short Int16S;
 
+
+
+//ENTERO 8-BITS EXACTO
+typedef  int8_t Int8T;
+
+//ENTERO 16-BITS EXACTO
+typedef  int16_t Int16T;
+
+//ENTERO 32-BITS EXACTO
+typedef  int16_t Int32T;
+
 //ENTERO 32 NO SIGNADO
 typedef unsigned int Int32U;
 
 //ENTERO 32 SIGNADO
 typedef signed int Int32S;
+
+
 
 //VALOR FLOTANTE 32-BITS
 typedef float Float32;
