cf66cb9aa19e4a5d6ae5b275f86e7e344b6591c9
Leonel Dan Fishkel <lfishkel@truelgicsoftware.com>
2014-10-05 20:18:06 -0300

    #31

--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -2,7 +2,12 @@
 #define CPU_H_
 
 #include <src/fereTypes.h>
+#include <src/fereStream.h>
+#include <src/fereTcb.h>
 
 Tcb* getCurrentTcb();
+StrCpuKer* getSCK();
+StrKerCpu* getSKC();
+
 
 #endif /* CPU_H_ */
